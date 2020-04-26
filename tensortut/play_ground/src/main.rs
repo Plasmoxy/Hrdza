0b63736e886dd63b99282e4169953b2a4ac8f034
Plasmoxy <sebohacker@gmail.com>
2020-04-26 22:55:09 +0200

    Update main.rs

--- tensortut/play_ground/src/main.rs
+++ tensortut/play_ground/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, nibba.");
+
+    let x: u32 = 5;
+    println!("x = {}", x);
+
+    // tuples
+    let v: (i64, i64) = (42, 33);
+    let (_, y) = v;
+    println!("y = {}", y);
+
+    // arrays
+    let a = [1, 2, 3, 4];
+    println!("a[2] = {:?}", a[2]);
+
+
 }
