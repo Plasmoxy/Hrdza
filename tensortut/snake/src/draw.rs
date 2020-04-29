ed1608c4be371bb0afb98e87ee2e5030de7fb8d9
Plasmoxy <sebohacker@gmail.com>
2020-04-29 13:10:05 +0200

    yes

--- tensortut/snake/src/draw.rs
+++ tensortut/snake/src/draw.rs
@@ -3,13 +3,14 @@ use piston_window::{rectangle, Context, G2d};
 
 const BLOCK_SIZE: f64 = 25.0;
 
-pub fn to_window_coord(coord: i32) -> f64 {
-  (coord as f64) * BLOCK_SIZE
+pub fn to_coord(game_coord: i32) -> f64 {
+  (game_coord as f64) * BLOCK_SIZE
 }
 
+
 pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
-  let gui_x = to_window_coord(x);
-  let gui_y = to_window_coord(y);
+  let gui_x = to_coord(x);
+  let gui_y = to_coord(y);
   rectangle(
     color,
     [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
@@ -27,8 +28,8 @@ pub fn draw_rectangle(
   con: &Context,
   g: &mut G2d,
 ) {
-  let gui_x = to_window_coord(x);
-  let gui_y = to_window_coord(y);
+  let gui_x = to_coord(x);
+  let gui_y = to_coord(y);
   rectangle(
     color,
     [
--- tensortut/snake/src/main.rs
+++ tensortut/snake/src/main.rs
@@ -1,7 +1,16 @@
 mod draw;
 mod snake;
+mod game;
+
+use piston_window::*;
+use piston_window::types::Color;
+use game::Game;
+use draw::to_coord;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0]; 
 
 fn main() {
-    println!("Hello, world!");
-    draw::hello_draw_module();
+    let (W, H) = (20, 20);
+    
+    
 }
