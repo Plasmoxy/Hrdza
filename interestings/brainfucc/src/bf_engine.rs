e477844094fd68a641c770ce980ae260af5f9f02
Plasmoxy <sebohacker@gmail.com>
2020-05-02 17:49:42 +0200

    bruh

--- interestings/brainfucc/src/bf_engine.rs
+++ interestings/brainfucc/src/bf_engine.rs
@@ -1,4 +1,3 @@
-
 pub const BLOCK_SIZE: usize = 30000;
 
 #[derive(PartialEq)]
@@ -10,10 +9,10 @@ pub enum EntryType {
 
 pub struct BrainfuckEngine {
   block: [i32; BLOCK_SIZE], // block of i32 (not bytes, i want more power)
-  ip: usize, // instruction pointer in source file
-  bp: usize, // block pointer inside block (current cell index)
-  loops: [usize; 255], // array of nested loop pointers
-  lp: usize, // current loop is lp-1, lp==0 means no current loop
+  ip: usize,                // instruction pointer in source file
+  bp: usize,                // block pointer inside block (current cell index)
+  loops: [usize; 255],      // array of nested loop pointers
+  lp: usize,                // current loop is lp-1, lp==0 means no current loop
 }
 
 impl BrainfuckEngine {
@@ -29,28 +28,58 @@ impl BrainfuckEngine {
   }
   
   /// Execute source string.
-  pub fn execute(&self, source: &str) {
+  pub fn execute(&mut self, source: &str) {
     let mut line_idx: u64 = 0; // track line index
-    
-    for line in source.lines() {
-      
-      // First, parse utility macros and functions
-      // -> Should be able to expand them into normal bf later.
-      // -> Shouldn't produce any errors !!!
-      // # 0 10 -> display block from 0 to 10
-      if line.starts_with("# ") {
-        let args_str: &str = &line[2..];
-        let args: Vec<&str> = args_str.split(" ").collect();
-        if (args.len() == 2) {
-          let from: usize = (args.get(0).unwrap()).parse().unwrap();
-          let to: usize = (args.get(1).unwrap()).parse().unwrap();
-          self.display_block(from, to);
+    let s_len = source.len();
+    let chars: Vec<char> = source.chars().collect();
+    // TODO: code for parsing args
+    // let args_str: &str = &line[2..];
+    // let args: Vec<&str> = args_str.split(" ").collect();
+    // if (args.len() == 2) {
+    //   let from: usize = (args.get(0).unwrap()).parse().unwrap();
+    //   let to: usize = (args.get(1).unwrap()).parse().unwrap();
+    //   self.display_block(from, to);
+    // }
+    while self.ip < s_len {
+      let instruction = chars.get(self.ip).unwrap();
+      // execute instructions
+      match instruction {
+        // basic Brainfuck instruction set
+        '>' => self.bp += 1,
+        '<' => self.bp -= 1,
+        '+' => self.block[self.bp] += 1,
+        '-' => self.block[self.bp] -= 1,
+
+        '[' => {
+          self.loops[self.lp] = self.ip;
+          self.lp += 1;
         }
+
+        ']' => {
+          if self.lp == 0 {
+            println!("Error: tried to close loop while not inside any.");
+            return;
+          }
+          if self.block[self.bp] != 0 {
+            self.ip = self.loops[self.lp - 1];
+          } else {
+            self.lp -= 1;
+          }
+        }
+
+        '.' => print!(
+          "{}",
+          std::char::from_u32(self.block[self.bp] as u32).unwrap_or('$')
+        ),
+
+        // custom macros / extensions (ignored by normal bf)
+        '#' => self.display_block(0, 10),
+        
+        _ => {}
+        
       }
       
-      
-      
-      line_idx += 1;
+      self.ip += 1;
     }
   }
   
@@ -58,18 +87,16 @@ impl BrainfuckEngine {
   pub fn display_block(&self, from: usize, to: usize) {
     let mut ss = String::from(format!("# {}=[", from));
     for i in from..=to {
-        ss.push_str(& if i == self.bp {
-            format!("({})", self.block[i])
-        } else {
-            format!("{}", self.block[i])
-        });
-        if i != to {
-            ss.push_str(", ")
-        }
+      ss.push_str(&if i == self.bp {
+        format!("({})", self.block[i])
+      } else {
+        format!("{}", self.block[i])
+      });
+      if i != to {
+        ss.push_str(", ")
+      }
     }
     ss.push_str(&format!("]={}", to));
     println!("{}", ss);
   }
-  
 }
-
