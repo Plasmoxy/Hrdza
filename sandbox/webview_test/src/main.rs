c79d6c2b4685de9f8ab125c672bf86eb0833788c
Plasmoxy <sebohacker@gmail.com>
2020-05-04 10:10:19 +0200

    hmhm

--- sandbox/webview_test/src/main.rs
+++ sandbox/webview_test/src/main.rs
@@ -1,11 +1,11 @@
 use web_view::*;
 
 fn main() {
-    let html_content = "<html><body><h1>Hello, World!</h1></body></html>";
+    let html = include_str!("index.html");
 	
     web_view::builder()
         .title("My Project")
-        .content(Content::Html(html_content))
+        .content(Content::Html(html))
         .size(320, 480)
         .resizable(false)
         .debug(true)
@@ -13,4 +13,6 @@ fn main() {
         .invoke_handler(|_webview, _arg| Ok(()))
         .run()
         .unwrap();
+        
+    
 }
